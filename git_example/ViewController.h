07c605d70d69b362e89c81a5426a2c6e86377370
Your Name <moshe@telemessage.com>
2017-01-15 15:47:46 +0200

    test

---
+++ git_example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_example
+//
+//  Created by Moshe Mairanz on 15/01/2017.
+//  Copyright © 2017 Moshe Mairanz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git_example/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  git_example
+//
+//  Created by Moshe Mairanz on 15/01/2017.
+//  Copyright © 2017 Moshe Mairanz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
