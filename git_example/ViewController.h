ee2d04aa49300ad5c37f33d150edcff10081a315
Moshe Mairanz <moshe@telemessage.com>
2017-01-30 11:00:14 +0200

    7

--- git_example/ViewController.h
+++ git_example/ViewController.h
@@ -5,7 +5,7 @@
 //  Created by Moshe Mairanz on 15/01/2017.
 //  Copyright © 2017 Moshe Mairanz. All rights reserved.
 //
-//4
+//7
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
